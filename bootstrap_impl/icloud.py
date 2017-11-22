9a6d0c58ad415357dee648764cffd7694c3badcb
Andreas Hartl <mac-tools@nd-com.net>
2017-11-22 13:15:14 +0100

    Moved iCloud-Login retrieval into tools

--- bootstrap_impl/icloud.py
+++ bootstrap_impl/icloud.py
@@ -1,23 +1,15 @@
 from .action import action
 from . import path, tools
-import getpass
-
-username = getpass.getuser()
     
 @action(
     username="The OS's user name to check"
 )
-def icloud(username=username):
+def icloud(username=tools.username):
     """
     Opens the iCloud preference pane for the user and tabs to the first entry if the user isn't signed in into iCloud.
     """
     try:
-        import subprocess
-        dscl = subprocess.check_output(["/usr/bin/env", "dscl", ".", "readpl", "/Users/{}".format(username), "dsAttrTypeNative:LinkedIdentity", "appleid.apple.com:linked identities"])
-        plist = dscl.split("\n", 1)[1]
-        import plistlib
-        appleid = plistlib.readPlistFromString(plist)
-        print("Signed in {} with Apple ID {}.".format(username, appleid[0]["full name"]))
+        print("Signed in {} with Apple ID {}.".format(username, tools.get_appleid(username)))
     except:
         print("Could not get Apple ID for {}. Opening System Preferences".format(username))
         tools.run("osascript", "-e", """
--- bootstrap_impl/tools.py
+++ bootstrap_impl/tools.py
@@ -1,7 +1,18 @@
-import subprocess
+import subprocess, plistlib, getpass
+
+username = getpass.getuser()
 
 def run(command, *args):
     returncode = subprocess.call(["/usr/bin/env", command] + list(args))
     if returncode in (126, 127):
         raise NameError("{} does not exist.".format(command))
     return returncode
+
+def get_appleid(user=None):
+    if not user:
+        user = username
+    dscl = subprocess.check_output(["/usr/bin/env", "dscl", ".", "readpl", "/Users/{}".format(user), "dsAttrTypeNative:LinkedIdentity", "appleid.apple.com:linked identities"])
+    plist = dscl.split("\n", 1)[1]
+    appleid = plistlib.readPlistFromString(plist)
+    return appleid[0]["full name"]
+    
