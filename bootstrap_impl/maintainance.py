fa0a0d460f81f0673a86be97b0a30ab8b9580277
Andreas Hartl <mac-tools@nd-com.net>
2018-03-02 15:21:58 +0100

    Added location action to bootstrap

--- bootstrap_impl/maintainance.py
+++ bootstrap_impl/maintainance.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from .action import *
 from . import path, tools
 
@@ -17,3 +19,11 @@ def update():
     os.chdir(path.scriptdir)
     tools.run("git", "pull")
     
+@action()
+def location():
+    """
+    Prints the location of bootstrap so that you can cd there with `cd $(bootstrap location)`
+    """
+    import sys
+    end = "\n" if sys.stdout.isatty() else ""
+    print(path.scriptdir, end=end)
