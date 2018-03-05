d703c1041eab9742a13616b5ae736f13e2019c77
Andreas Hartl <mac-tools@nd-com.net>
2018-03-05 16:09:50 +0100

    Separated requirements for Python itself and for xonsh

--- bootstrap_impl/pip.py
+++ bootstrap_impl/pip.py
@@ -2,6 +2,8 @@ from .action import *
 from . import path, tools
 
 requirements = path.configfiles("requirements.txt")
+xonsh_requirements = path.configfiles("xonsh-requirements.txt")
+
 
 @default
 @action(
@@ -25,9 +27,37 @@ def pip(requirements=requirements):
         tools.run(*args)
 
 @default
+@action(
+    xonsh_requirements="The files used for the requirements"
+)
+def xpip(xonsh_requirements=xonsh_requirements):
+    """
+    Installs Xonsh Python 3 modules via xpip as defined in the requirements file.
+    """
+    existingRequirements = path.existing(path.expandusers(xonsh_requirements))
+    args = ["xpip", "install"]
+    for requirement in existingRequirements:
+        args.append("--requirement")
+        args.append("'{}'".format(requirement))
+    tools.run("xonsh", "-c", " ".join(args))
+
+@default
 @action
 def pipdate():
     """
     Update existing Python packages. Requires the python egg pipdate.
     """
     tools.run("pipdate3")
+
+@default
+@action
+def xpipdate():
+    """
+    Update existing Xonsh Python packages. Requires the python egg pipdate.
+    """
+    import subprocess, re
+    
+    versions = subprocess.check_output(['/usr/bin/env', 'xonsh', '-c', 'xpip freeze --local'])
+    entries = re.findall("^(.*?)=", versions, re.MULTILINE)
+    for entry in entries:
+        tools.run("xonsh", "-c", "xpip install -U {}".format(entry))
